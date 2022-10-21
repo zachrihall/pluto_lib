6c40151485adc7241aebec0087d1dc2354fe6b0c
macapedi <macapedi90@gmail.com>
2022-10-21 19:36:23 -0400

    form  completed

--- form.js
+++ form.js
@@ -1,24 +1,35 @@
 function css(e, styles) {
-    for (const property in styles)
-        e.style[property] = styles[property];
+  for (const property in styles) e.style[property] = styles[property];
 }
 
+export function createForm(labelNames, container) {
 
-
-export function createForm(str) {
+  const parentContainer = document.getElementById(container);
   const form = document.createElement("form");
   document.body.appendChild(form);
-  css(form, { display: 'flex', 'flex-direction': 'column'});
-  const formHolder = document.querySelector("form");
-  const label = document.createElement("label");
-  formHolder.appendChild(label);
-  const labelText = document.createTextNode(str);
-  label.appendChild(labelText);
-  const input = document.createElement("input");
-  input.setAttribute("type", "text");
-  formHolder.appendChild(input);
-  css(form, { width: '30%'});
+  css(form, { display: "flex", "flex-direction": "column" });
+  labelNames.forEach((labelName) => {
+    const formHolder = document.querySelector("form");
+
+    const label = document.createElement("label");
+    formHolder.appendChild(label);
+    const labelText = document.createTextNode(labelName);
+    label.appendChild(labelText);
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    formHolder.appendChild(input);
+    css(input, { width: "30%" });
+  });
+  const button = document.createElement("button");
+  css(button, {
+    width: "20%",
+    margin: "1rem",
+    "background-color": "AliceBlue",
+  });
+  button.innerText = "Submit";
+
+  form.appendChild(button);
+  parentContainer.appendChild(form);
 }
 
-
-createForm("Full Name");
+// createForm(["Name", "Last Name"]);
