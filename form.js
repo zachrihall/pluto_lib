85d72b6f008f70e27aff476d269930554c6b4b8d
macapedi <macapedi90@gmail.com>
2022-10-21 19:09:23 -0400

    basic input in form done and styled

--- form.js
+++ form.js
@@ -1,11 +1,24 @@
-export function createForm(str) {
-  const form = document.createElement("form");
-  document.body.appendChild(form);
-  const formHolder = document.querySelector("form");
-  const input = document.createElement("input");
-  input.setAttribute("type", "text");
-  formHolder.appendChild(input);
+function css(e, styles) {
+    for (const property in styles)
+        e.style[property] = styles[property];
 }
 
 
-createForm("me");
+
+export function createForm(str) {
+  const form = document.createElement("form");
+  document.body.appendChild(form);
+  css(form, { display: 'flex', 'flex-direction': 'column'});
+  const formHolder = document.querySelector("form");
+  const label = document.createElement("label");
+  formHolder.appendChild(label);
+  const labelText = document.createTextNode(str);
+  label.appendChild(labelText);
+  const input = document.createElement("input");
+  input.setAttribute("type", "text");
+  formHolder.appendChild(input);
+  css(form, { width: '30%'});
+}
+
+
+createForm("Full Name");
