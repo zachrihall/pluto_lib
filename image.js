54a98ff847d59f13b53e7d3234082624d37eab56
zachrihall <hallzachri@gmail.com>
2022-10-21 17:23:16 -0400

    changed function name

--- image.js
+++ image.js
@@ -1,12 +1,5 @@
-// source for image: https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Soccerball.svg/640px-Soccerball.svg.png
 
-
-// const para = document.createElement("p");
-// para.innerText = "This is a paragraph";
-// document.body.appendChild(para);
-src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Soccerball.svg/640px-Soccerball.svg.png'
-
-function makeSoccerBall(src, width, name) {
+function image(src, width, name) {
     const img = document.createElement('img');
     img.style.width = `${width}px`;
     img.src = src;
@@ -14,9 +7,6 @@ function makeSoccerBall(src, width, name) {
     document.body.appendChild(img);
 }
 
-// makeSoccerBall(src, 40, 'my-image');
+module.exports = {image}
 
-// module.exports = {makeSoccerBall}
-
-// export default makeSoccerBall;
 
